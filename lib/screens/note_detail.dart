c26fe96b246e0837030aceb6e2f66c57ae61050c
Alfred Sopia <asopia@strathmore.edu>
2020-06-14 20:31:54 +0300

    Defined callback function for the device and app back button

--- lib/screens/note_detail.dart
+++ lib/screens/note_detail.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 
 class NoteDetail extends StatefulWidget {
   String appBarTitle;
+
   NoteDetail(this.appBarTitle);
+
   @override
   State<StatefulWidget> createState() {
     return NoteDetailState(this.appBarTitle);
@@ -15,130 +17,147 @@ class NoteDetailState extends State {
   static var _priorities = ['High', 'Low'];
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
+
   NoteDetailState(this.appBarTitle);
+
   @override
   Widget build(BuildContext context) {
     TextStyle textStyle = Theme.of(context).textTheme.title;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(appBarTitle),
-      ),
-      body: Padding(
-        padding: EdgeInsets.only(
-          top: 15.0,
-          left: 10.0,
-          right: 10.0,
-        ),
-        child: ListView(
-          children: <Widget>[
-            /// first element
-            ListTile(
-              title: DropdownButton(
-                items: _priorities.map((String dropDownStringItem) {
-                  return DropdownMenuItem<String>(
-                    value: dropDownStringItem,
-                    child: Text(dropDownStringItem),
-                  );
-                }).toList(),
-                style: textStyle,
-                value: 'Low',
-                onChanged: (valueSelectedByUser) {
-                  setState(() {
-                    debugPrint('User selected $valueSelectedByUser');
-                  });
-                },
-              ),
+    return WillPopScope(
+        onWillPop: () {
+          ///code to control what happens when user press the device back button
+          moveToLastScreen();
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(appBarTitle),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                ///code to control what happens when user press the app back button
+                moveToLastScreen();
+              },
             ),
-
-            /// second element
-            Padding(
-              padding: EdgeInsets.only(
-                top: 15.0,
-                left: 10.0,
-                right: 10.0,
-              ),
-              child: TextField(
-                controller: titleController,
-                style: textStyle,
-                onChanged: (value) {
-                  debugPrint('Something chnaged in Title Text field');
-                },
-                decoration: InputDecoration(
-                    labelText: 'Title',
-                    labelStyle: textStyle,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0))),
-              ),
+          ),
+          body: Padding(
+            padding: EdgeInsets.only(
+              top: 15.0,
+              left: 10.0,
+              right: 10.0,
             ),
-
-            /// Third element
-            Padding(
-              padding: EdgeInsets.only(
-                top: 15.0,
-                left: 10.0,
-                right: 10.0,
-              ),
-              child: TextField(
-                controller: descriptionController,
-                style: textStyle,
-                onChanged: (value) {
-                  debugPrint('Something chnaged in Description Text field');
-                },
-                decoration: InputDecoration(
-                    labelText: 'Description',
-                    labelStyle: textStyle,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0))),
-              ),
-            ),
-
-            /// 4th element
-            Padding(
-              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: RaisedButton(
-                      color: Theme.of(context).primaryColorDark,
-                      textColor: Theme.of(context).primaryColorLight,
-                      child: Text(
-                        'Save',
-                        textScaleFactor: 1.5,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          debugPrint("Save button clicked");
-                        });
-                      },
-                    ),
+            child: ListView(
+              children: <Widget>[
+                /// first element
+                ListTile(
+                  title: DropdownButton(
+                    items: _priorities.map((String dropDownStringItem) {
+                      return DropdownMenuItem<String>(
+                        value: dropDownStringItem,
+                        child: Text(dropDownStringItem),
+                      );
+                    }).toList(),
+                    style: textStyle,
+                    value: 'Low',
+                    onChanged: (valueSelectedByUser) {
+                      setState(() {
+                        debugPrint('User selected $valueSelectedByUser');
+                      });
+                    },
                   ),
+                ),
 
-                  Container(
-                    width: 5.0,
+                /// second element
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 15.0,
+                    left: 10.0,
+                    right: 10.0,
                   ),
+                  child: TextField(
+                    controller: titleController,
+                    style: textStyle,
+                    onChanged: (value) {
+                      debugPrint('Something chnaged in Title Text field');
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'Title',
+                        labelStyle: textStyle,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0))),
+                  ),
+                ),
 
-                  ///second sublement in the row element
-                  Expanded(
-                    child: RaisedButton(
-                      color: Theme.of(context).primaryColorDark,
-                      textColor: Theme.of(context).primaryColorLight,
-                      child: Text(
-                        'Delete',
-                        textScaleFactor: 1.5,
+                /// Third element
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 15.0,
+                    left: 10.0,
+                    right: 10.0,
+                  ),
+                  child: TextField(
+                    controller: descriptionController,
+                    style: textStyle,
+                    onChanged: (value) {
+                      debugPrint('Something chnaged in Description Text field');
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'Description',
+                        labelStyle: textStyle,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0))),
+                  ),
+                ),
+
+                /// 4th element
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: RaisedButton(
+                          color: Theme.of(context).primaryColorDark,
+                          textColor: Theme.of(context).primaryColorLight,
+                          child: Text(
+                            'Save',
+                            textScaleFactor: 1.5,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              debugPrint("Save button clicked");
+                            });
+                          },
+                        ),
                       ),
-                      onPressed: () {
-                        setState(() {
-                          debugPrint("Delete button clicked");
-                        });
-                      },
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+
+                      Container(
+                        width: 5.0,
+                      ),
+
+                      ///second sublement in the row element
+                      Expanded(
+                        child: RaisedButton(
+                          color: Theme.of(context).primaryColorDark,
+                          textColor: Theme.of(context).primaryColorLight,
+                          child: Text(
+                            'Delete',
+                            textScaleFactor: 1.5,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              debugPrint("Delete button clicked");
+                            });
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+  void moveToLastScreen(){
+    Navigator.pop(context);
   }
 }
